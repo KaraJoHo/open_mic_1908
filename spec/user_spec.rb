91612cb618f5548f97b9415c68e61cc476684c0a
KaraJH <karas.jones@yahoo.com>
2022-12-25 13:12:08 -0700

    test: Tell method

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,6 +3,7 @@ require './lib/joke'
 
 RSpec.describe User do 
   let(:sal) {User.new("Sal")}
+  let(:ali) {User.new("Ali")}
   let(:joke_1) {Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")}
   let(:joke_2) {Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")}
   describe '#initialize' do 
@@ -19,6 +20,15 @@ RSpec.describe User do
       sal.learn(joke_2)
       
       expect(sal.jokes).to eq([joke_1, joke_2])
+    end
+  end
+
+  describe '#tell' do 
+    it 'can tell another user a joke and that user has new jokes added to their list' do 
+      sal.tell(ali, joke_1)
+      sal.tell(ali, joke_2)
+
+      expect(ali.jokes).to eq([joke_1, joke_2])
     end
   end
 end
