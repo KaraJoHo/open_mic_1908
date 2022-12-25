48fd42c41acd22a6a32a667210a9b6556f6eb1b7
KaraJH <karas.jones@yahoo.com>
2022-12-25 14:44:51 -0700

    feat: Repeated jokes method and IT4 complete

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -10,4 +10,19 @@ class OpenMic
   def welcome(performer)
     @performers << performer
   end
+
+  def repeated_jokes? 
+   
+    if all_jokes.uniq == all_jokes
+      false
+    else 
+      true
+    end
+  end
+
+ def all_jokes 
+  @performers.flat_map do |performer| 
+    performer.jokes
+  end
+ end
 end
